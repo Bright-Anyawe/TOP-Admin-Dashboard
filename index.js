d738b751cba56c03b0eaa4fc2c1f1ae56f07fe2b
Bright-Anyawe <157108952+Bright-Anyawe@users.noreply.github.com>
2024-08-15 14:39:29 +0000

    Refactor: Design admin DB mobile layout

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const toggleIconMenu = document.querySelector("#toggleMenuIcon");
 const sideBar = document.querySelector("#side-bar");
 
 function toggleIcon() {
-    toggleIconMenu.classList.toggle("active");
-    sideBar.classList.toggle('active')
+  toggleIconMenu.classList.toggle("active");
+  sideBar.classList.toggle("active");
 }
 
 toggleIconMenu.addEventListener("click", toggleIcon);
+
+function removeSideBarOnPageClick(e) {
+  if (e.target !== toggleIconMenu && e.target !== sideBar) {
+    toggleIconMenu.classList.remove("active");
+    sideBar.classList.remove("active");
+  }
+}
+document.addEventListener("click", removeSideBarOnPageClick);
