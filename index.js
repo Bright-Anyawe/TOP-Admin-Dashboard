8aefce07eb35ccfff267f22fe609695c2475edb6
Bright-Anyawe <157108952+Bright-Anyawe@users.noreply.github.com>
2024-08-15 17:37:10 +0000

    Refactor: Design the layout for the header

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ window.onscroll = function () {
 
   if (prevScrollPos > currentScrollPos) {
     
-    document.querySelector("#toggleMenuIcon").style.top = "24px";
+    document.querySelector("#toggleMenuIcon").style.top = "30";
   } else {
     
     document.querySelector("#toggleMenuIcon").style.top = "-80px";
